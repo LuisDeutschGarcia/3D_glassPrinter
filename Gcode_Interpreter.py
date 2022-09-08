25fbadd67c5276b87a3740fdc272fe180289a96e
LuisDeutschGarcia <104651857+LuisDeutschGarcia@users.noreply.github.com>
2022-09-08 13:34:00 -0400

    Update Gcode_Interpreter.py

--- Gcode_Interpreter.py
+++ Gcode_Interpreter.py
@@ -1,169 +1,143 @@
-from G02 import *
+import re
+from circleInterpolation import *
 import numpy as np
 import plotly.graph_objects as go
 
-g_code = "G01 X0.0 Y0.0 Z0.0" \
-         "G01 X9.0" \
-         "G03 Y2.0 X11.0 R2.0" \
-         "G01 Y11.0" \
-         "G03 X9.0 Y13.0 R2.0" \
-         "G01 X0.0" \
-         "G03 X-2.0 Y11.0 R2.0" \
-         "G01 Y2.0" \
-         "G03 X0.0 Y0.0 R2.0"
+# path = r'C:\Users\luisd\Downloads\TecLogo_0004.ngc'
 
-# g_code =  "G01 X0 Y0" \
-#           "G01 Z-0.25" \
-#           "G02 X0.15 Y0 R0.075" \
-#           "G02 X-0.15 Y0 R0.15" \
-#           "G02 X0.3 Y0 R0.225" \
-#           "G02 X-0.3 Y0 R0.3" \
-#           "G02 X0.45 Y0 R0.375" \
-#           "G02 X-0.45 Y0 R0.45" \
-#           "G02 X0.6 Y0 R0.525" \
-#           "G02 X-0.6 Y0 R0.6" \
-#           "G02 X0.75 Y0 R0.675" \
-#           "G02 X-0.75 Y0 R0.75" \
-#           "G02 X0.9 Y0 R0.825" \
-#           "G02 X-0.9 Y0 R0.9" \
-#           "G02 X1.05 Y0 R0.975" \
-#           "G02 X-1.05 Y0 R1.05" \
-#           "G02 X1.125 Y0 R1.0875" \
-#           "G02 X-1.125 Y0 R1.125" \
-#           "G02 X1.125 Y0 R1.125" \
-#           "G02 X0.75 Y-0.375 R0.375" \
-#           "G01 Z0.1" \
-#           "G01 X0 Y0"  \
-#           "G01 Z-0.5" \
-#           "G02 X0.15 Y0 R0.075" \
-#           "G02 X-0.15 Y0 R0.15" \
-#           "G02 X0.3 Y0 R0.225" \
-#           "G02 X-0.3 Y0 R0.3" \
-#           "G02 X0.45 Y0 R0.375" \
-#           "G02 X-0.45 Y0 R0.45" \
-#           "G02 X0.6 Y0 R0.525" \
-#           "G02 X-0.6 Y0 R0.6" \
-#           "G02 X0.75 Y0 R0.675"  \
-#           "G02 X-0.75 Y0 R0.75" \
-#           "G02 X0.9 Y0 R0.825" \
-#           "G02 X-0.9 Y0 R0.9" \
-#           "G02 X1.05 Y0 R0.975"  \
-#           "G02 X-1.05 Y0 R1.05"  \
-#           "G02 X1.125 Y0 R1.0875" \
-#           "G02 X-1.125 Y0 R1.125" \
-#           "G02 X1.125 Y0 R1.125" \
-#           "G02 X0.75 Y-0.375 R0.375"  \
-#           "G01 Z0.1" \
-#           "G01 X0 Y0" \
-#           "G01 Z-0.75" \
-#           "G02 X0.15 Y0 R0.075"  \
-#           "G02 X-0.15 Y0 R0.15" \
-#           "G02 X0.3 Y0 R0.225" \
-#           "G02 X-0.3 Y0 R0.3" \
-#           "G02 X0.45 Y0 R0.375" \
-#           "G02 X-0.45 Y0 R0.45" \
-#           "G02 X0.6 Y0 R0.525" \
-#           "G02 X-0.6 Y0 R0.6" \
-#           "G02 X0.75 Y0 R0.675" \
-#           "G02 X-0.75 Y0 R0.75" \
-#           "G02 X0.9 Y0 R0.825" \
-#           "G02 X-0.9 Y0 R0.9" \
-#           "G02 X1.05 Y0 R0.975" \
-#           "G02 X-1.05 Y0 R1.05" \
-#           "G02 X1.125 Y0 R1.0875" \
-#           "G02 X-1.125 Y0 R1.125" \
-#           "G02 X1.125 Y0 R1.125" \
-#           "G02 X0.75 Y-0.375 R0.375" \
-#           "G01 Z0.1" \
-#           "G01 X0 Y0" \
-#           "G01 Z-0.9" \
-#           "G02 X0.15 Y0 R0.075" \
-#           "G02 X-0.15 Y0 R0.15" \
-#           "G02 X0.3 Y0 R0.225" \
-#           "G02 X-0.3 Y0 R0.3" \
-#           "G02 X0.45 Y0 R0.375" \
-#           "G02 X-0.45 Y0 R0.45" \
-#           "G02 X0.6 Y0 R0.525" \
-#           "G02 X-0.6 Y0 R0.6" \
-#           "G02 X0.75 Y0 R0.675" \
-#           "G02 X-0.75 Y0 R0.75" \
-#           "G02 X0.9 Y0 R0.825" \
-#           "G02 X-0.9 Y0 R0.9" \
-#           "G02 X1.05 Y0 R0.975" \
-#           "G02 X-1.05 Y0 R1.05" \
-#           "G02 X1.125 Y0 R1.0875" \
-#           "G02 X-1.125 Y0 R1.125" \
-#           "G02 X1.125 Y0 R1.125" \
-#           "G02 X0.75 Y-0.375 R0.375" \
-#           "G01 Z0.1"
+def get_point(char,s):
+    res = re.split('([-+]?\d+\.\d+)|([-+]?\d+)', s.strip())
+    res_f = [r.strip() for r in res if r is not None and r.strip() != '']
+    return float(res_f[res_f.index(char)+1])
 
 
-arr_gCode = g_code.split('G')
-print(arr_gCode)
+def arcInterpolation_3(x1, x2, y1, y2, R, I, J, resolution = 50, G02=True):
+    Chord = np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
-x = []
-y = []
-z = []
-command = []
-radius = []
-for element in arr_gCode:
-    try:
-        if element != '':
-            command.append(element.split(' ')[0])
-        if 'R' in element:
-            radius.append(float(element.split('R')[1]))
-        elif 'R' not in element and element != '':
-            radius.append(0)
-        if "X" in element and "Y" in element and "Z" in element:
-            x.append(float(element.split('X')[1].split(' ')[0]))
-            y.append(float(element.split('Y')[1].split(' ')[0]))
-            z.append(float(element.split('Z')[1].split(' ')[0]))
-        elif "X" in element and "Y" in element:
-            x.append(float(element.split('X')[1].split(' ')[0]))
-            y.append(float(element.split('Y')[1].split(' ')[0]))
-            z.append(z[-1])
-        elif "X" in element:
-            x.append(float(element.split('X')[1].split(' ')[0]))
-            y.append(y[-1])
-            z.append(z[-1])
-        elif "Y" in element:
-            x.append(x[-1])
-            y.append(float(element.split('Y')[1].split(' ')[0]))
-            z.append(z[-1])
-        elif "Z" in element:
-            x.append(x[-1])
-            y.append(y[-1])
-            z.append(float(element.split('Z')[1].split(' ')[0]))
-    except:
-        pass
+    if R == 0:
+        Xc, Yc = x1 + I, y1 + J
+        R = np.sqrt((x1 - Xc) ** 2 + (y1 - Yc) ** 2)
+        IJ = True
+    else:
+        Xchord = (x2 + x1) / 2
+        Ychord = (y2 + y1) / 2
+        slope = np.sqrt(R ** 2 - (Chord / 2) ** 2)
+        Run = (np.abs(x1 - x2) / 2 * slope) / (Chord / 2)
+        Rise = (np.abs(y2 - y1) / 2 * Run) / (np.abs(x1 - x2) / 2)
+        IJ = False
 
-print(np.array([[cmd, x, y, z, rad] for cmd, x, y, z, rad in zip(command, x, y, z, radius)]))
+    alpha = np.arccos(1 - (Chord ** 2) / (2 * (R ** 2)))
+    beta = alpha / resolution
 
-coordinates = []
-rslt = 10
-for c in range(len(command)):
-    try:
-        coordinates.append([x[c], y[c], z[c]])
-        if command[c + 1] == "03":
-            i, j = arcInterpolation(x[c], x[c + 1], y[c], y[c + 1], radius[c + 1], resolution=rslt, G02=False)
-            for x_c, y_c in zip(i, j):
-                coordinates.append([x_c, y_c, z[c]])
-        elif command[c + 1] == "02":
-            i, j = arcInterpolation(x[c], x[c + 1], y[c], y[c + 1], radius[c + 1], resolution=rslt, G02=True)
-            for x_c, y_c in zip(i, j):
-                coordinates.append([x_c, y_c, z[c]])
-    except:
-        pass
+    if not (IJ):
+        if (G02 == True and (x1 < x2 and y1 < y2)) or (G02 == False and (x1 > x2 and y1 > y2)):
+            Xc = Xchord + Rise
+            Yc = Ychord - Run
+        elif (G02 == True and (x1 < x2 and y1 > y2)) or (G02 == False and (x1 > x2 and y1 < y2)):
+            Xc = Xchord - Rise
+            Yc = Ychord - Run
+        elif (G02 == True and (x1 > x2 and y1 > y2)) or (G02 == False and (x1 < x2 and y1 < y2)):
+            Xc = Xchord - Rise
+            Yc = Ychord + Run
+        elif (G02 == True and (x1 > x2 and y1 < y2)) or (G02 == False and (x1 < x2 and y1 > y2)):
+            Xc = Xchord + Rise
+            Yc = Ychord + Run
+        elif (G02 == True and (x1 == x2 and y1 < y2)) or (G02 == False and (x1 == x2 and y1 > y2)):
+            Xc = Xchord + slope
+            Yc = Ychord
+        elif (G02 == True and (x1 == x2 and y1 > y2)) or (G02 == False and (x1 == x2 and y1 < y2)):
+            Xc = Xchord - slope
+            Yc = Ychord
+        elif (G02 == True and (x1 < x2 and y1 == y2)) or (G02 == False and (x1 > x2 and y1 == y2)):
+            Xc = Xchord
+            Yc = Ychord - slope
+        elif (G02 == True and (x1 > x2 and y1 == y2)) or (G02 == False and (x1 < x2 and y1 == y2)):
+            Xc = Xchord
+            Yc = Ychord + slope
 
-coordinates = np.array(coordinates)
+    Xdiff = x1 - Xc
+    Ydiff = y1 - Yc
 
-fig = go.Figure(data=[go.Scatter3d(x=coordinates[:, 0], y=coordinates[:, 1], z=coordinates[:, 2],
-                                   marker=dict(size=6, color=coordinates[:, 2], colorscale='electric'),
-                                   line=dict(color='darkblue', width=2))])
+    w = np.pi - np.arcsin(Ydiff / R) if Xdiff >= 0 else np.arcsin(Ydiff / R)
 
-# fig = go.Figure(data=[go.Scatter3d(x=coordinates[:, 0], y=coordinates[:, 1], z=coordinates[:, 2],
-#                                    mode='markers')])
+    if G02:
+        arc = np.array([[Xc - R * np.cos(w + (beta * n)),
+                         Yc + R * np.sin(w + (beta * n))] for n in range(0, resolution + 1)])
+    else:
+        arc = np.array([[Xc - R * np.cos(w - (beta * n)),
+                         Yc + R * np.sin(w - (beta * n))] for n in range(0, resolution + 1)])
 
-fig.show()
+    return arc[:, 0], arc[:, 1]
 
-print("Done")
+def gCode_interpreter(path, verbose=False):
+
+    command, X, Y, Z, R, I, J = [], [], [], [], [], [], []
+
+    g_code = np.array(open(path, 'r').read().split('\n'))
+
+    for element in g_code:
+        try:
+            if "G01" in element or "G02" in element or "G03" in element:
+                command.append(element.split(' ')[0])
+                if 'R' in element:
+                    R.append(get_point("R", element))
+                elif 'R' not in element and element != '':
+                    R.append(0)
+                if 'I' in element:
+                    I.append(get_point("I", element))
+                elif 'I' not in element and element != '':
+                    I.append(0)
+                if 'J' in element:
+                    J.append(get_point("J", element))
+                elif 'J' not in element and element != '':
+                    J.append(0)
+                if "X" in element and "Y" in element and "Z" in element:
+                    X.append(get_point("X", element))
+                    Y.append(get_point("Y", element))
+                    Z.append(get_point("Z", element))
+                elif "X" in element and "Y" in element:
+                    X.append(get_point('X', element))
+                    Y.append(get_point('Y', element))
+                    Z.append(Z[-1])
+                elif "X" in element:
+                    X.append(get_point('X', element))
+                    Y.append(Y[-1])
+                    Z.append(Z[-1])
+                elif "Y" in element:
+                    Y.append(get_point('Y', element))
+                    X.append(X[-1])
+                    Z.append(Z[-1])
+                elif "Z" in element:
+                    Z.append(get_point('Z', element))
+                    X.append(X[-1])
+                    Y.append(Y[-1])
+        except:
+            print(f"Exeception in: {element}")
+
+    rslt = 60
+    coordinates = []
+    for c in range(len(command)):
+        try:
+            coordinates.append([X[c], Y[c], Z[c]])
+            if command[c + 1] == "G03":
+                i, j = arcInterpolation_3(X[c], X[c + 1], Y[c], Y[c + 1], R[c + 1], I[c + 1], J[c + 1], resolution=rslt,
+                                          G02=False)
+                for x_c, y_c in zip(i, j):
+                    coordinates.append([x_c, y_c, Z[c]])
+            if command[c + 1] == "G02":
+                i, j = arcInterpolation_3(X[c], X[c + 1], Y[c], Y[c + 1], R[c + 1], I[c + 1], J[c + 1], resolution=rslt,
+                                          G02=True)
+                for x_c, y_c in zip(i, j):
+                    coordinates.append([x_c, y_c, Z[c]])
+        except:
+            pass
+
+    coordinates = np.array(coordinates)
+
+    if verbose:
+        fig = go.Figure(data=[go.Scatter3d(x=coordinates[:, 0], y=coordinates[:, 1], z=coordinates[:, 2],
+                                           marker=dict(size=6, color="darkblue", colorscale='electric'),
+                                           line=dict(color='slategray', width=2))])
+        fig.show()
+
+    return coordinates

78a870fc1d4902202bc3f02efead52ebac9ff5c8
LuisDeutschGarcia <104651857+LuisDeutschGarcia@users.noreply.github.com>
2022-09-08 13:34:51 -0400

    Update geometries.py

--- geometries.py
+++ geometries.py
@@ -26,22 +26,58 @@ def snake(no_lines=8, plot=False):
 
     return np.array([[x, y, z] for x, y, z in zip(x_s, y_s, [0] * no_lines * 2)])
 
+def specific_spiral():
+    radius = 6
+    circle = np.array([[radius * np.cos(angle), radius * np.sin(angle), 0] for angle in np.arange(0, 2 * np.pi, 0.05)])
+    x, y, z = list(circle[:,0]), list(circle[:,1]), list(circle[:,2])
+
+    sample = 400
+    z_s = list(np.linspace(0, 10, sample) * -2)
+    r_s = (np.linspace(0, 10, sample)) + radius
+    theta = np.linspace(0,8 * np.pi, sample)
+    for i in range(len(theta)):
+        x.append(r_s[i] * np.cos(theta[i]))
+        y.append(r_s[i] * np.sin(theta[i]))
+        z.append(z_s[i])
+
+    sprl = np.array([[X,Y,Z] for X,Y,Z in zip(x,y,z)])
+    return sprl
+
 
 def spiral(plot=False):
-    theta = np.linspace(0, 4 * np.pi, 75)
-    z_s = np.linspace(0, 1, 75)
-    r_s = (z_s ** 2 + 1) / 2
-    x_s = r_s * np.sin(theta)
-    y_s = r_s * np.cos(theta)
+    circle = np.array([[3 * np.cos(angle), 3 * np.sin(angle),0] for angle in np.arange(0, 2 * np.pi,0.05)])
+    sample = 250
+    theta = np.linspace(0, 15 * np.pi, sample)
+    z_s = np.linspace(0, 20, sample) * 2
+    r_s = (z_s / 8)
+    x_s = r_s * np.sin(theta) * 2
+    y_s = r_s * np.cos(theta) * 2
+
+    spiral = np.array([[x, y, z] for x, y, z in zip(x_s, y_s, z_s)])[:100, :]
 
     if plot:
         fig = px.scatter_3d(x=x_s, y=y_s, z=z_s)
         fig.show()
 
-    return np.array([[x, y, z] for x, y, z in zip(x_s, y_s, z_s)])
+    # return circle
+
+    return spiral
 
 
-def cube(height = 1, plot=False):
+def cilindrical_spiral(plot=False):
+    sample = 250
+    theta = np.linspace(0, 15 * np.pi, sample)
+    z_s = np.linspace(0, 40, sample) * -1
+    x_s = 5 * np.sin(theta) * 1.5
+    y_s = 5 * np.cos(theta) * 1.5
+    if plot:
+        fig = px.scatter_3d(x=x_s, y=y_s, z=z_s)
+        fig.show()
+
+    return np.array([[x, y, z] for x, y, z in zip(x_s, y_s, z_s)])[:100, :]
+
+
+def cube(height=1, plot=False):
     coordinates = np.array([[8, 0, 0],
                             [8, -8, 0],
                             [0, -8, 0],
@@ -86,3 +122,14 @@ def tecLogo():
                             [9.7, 3.87, 0.]])
 
     return coordinates
+
+def downSampling(coordinates, rate = 0.7):
+
+    downsample = int(rate * len(coordinates))
+    rows = np.sort(np.ndarray.flatten(np.random.randint(0, len(coordinates), size=(downsample, 1))))
+    coordinates = coordinates[rows]
+    new = [[0, 0, 0]]
+    for element in coordinates:
+        new.append(element)
+
+    return np.array(new)
